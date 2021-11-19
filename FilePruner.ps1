378e186731b0f94e6685709eceeefe71f163a0d6
iBowler1995 <93746898+iBowler1995@users.noreply.github.com>
2021-11-19 11:31:20 -0500

    Add files via upload

--- FilePruner.ps1
+++ FilePruner.ps1
@@ -30,17 +30,51 @@ param (
     [Int]$Threshold,
     [Parameter(Mandatory=$true)]
     [String]$Path
+    [Parameter(Mandatory = $false)]
+    [Switch]$File,
+    [Parameter(Mandatory = $false)]
+    [String]$FilePath
 )
 
+
 $Age=(Get-Date).AddDays(- $Threshold)
-#Recording our actions
-Start-Transcript -Path .\FilePrune.log -Force
-#Gets all items (and items in subdirectories, thanks to -recurse) where last write time is less than or equal to our $Age
-Get-ChildItem -Path $path -Recurse | Where-Object {($_.LastWriteTime -le $Age)} | #The pipline | lets you combine multiple actions into one command
-    Remove-Item -Force -Recurse -ErrorAction SilentlyContinue #We ignore errors because some files will throw an error if they're in use or locked
-#Deleting all empty folders afterwards
-Get-ChildItem -Path $Path -Recurse -Force |
-    Where-Object {$_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | #PsIsContainer is a boolean for whether an item is a directory or not. True = yes, False = no
-    Where-Object {!$_.PSIsContainer}) -eq $null } | #This line and the above get all items that are directories and have no files in them
-    Remove-Item -Force -Recurse
-Stop-Transcript
+
+If ($File){
+    If ($FilePath -eq $null)
+    {
+        "File switch used but no file path provided. Please provide file path or remove file switch and run again." | out-file .\ErrorLog -Append
+    }
+    else{
+        $Files = Get-Content $FilePath
+        Try{
+        foreach ($Line in $Files){
+            #Gets all items (and items in subdirectories, thanks to -recurse) where last write time is less than or equal to our $Age and writes any errors to a log file
+            Get-ChildItem -Path $path -Recurse | Where-Object {($_.LastWriteTime -le $Age)} | #The pipline | lets you combine multiple actions into one command
+                    Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+                }
+        Catch{
+             $Error | out-file .\ErrorLog.log -Append
+            } 
+            #Deleting all empty folders afterwards
+        Get-ChildItem -Path $Path -Recurse -Force |
+            Where-Object {$_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force | #PsIsContainer is a boolean for whether an item is a directory or not. True = yes, False = no
+            Where-Object {!$_.PSIsContainer}) -eq $null } | #This line and the above get all items that are directories and have no files in them
+            Remove-Item -Force -Recurse
+        }       
+    }
+
+    }
+else{
+    Try{
+    Get-ChildItem -Path $path -Recurse | Where-Object {($_.LastWriteTime -le $Age)} |
+        Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
+    }
+    Catch{
+        $Error | out-file .\ErrorLog.log -Append
+    } 
+
+    Get-ChildItem -Path $Path -Recurse -Force |
+        Where-Object {$_.PSIsContainer -and (Get-ChildItem -Path $_.FullName -Recurse -Force |
+        Where-Object {!$_.PSIsContainer}) -eq $null } |
+        Remove-Item -Force -Recurse   
+}
